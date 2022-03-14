a2b08a48f2385849317434abb5e8d0b82e22323e
Daniel Jimenez <danyfebrero@gmail.com>
2022-03-14 19:51:46 -0400

    lib cleaning

--- stockAnalizer.py
+++ stockAnalizer.py
@@ -1,10 +1,30 @@
-from multiprocessing import Value
 import pandas as pd
-import matplotlib
 import plotly.graph_objects as go
 import json, csv
 from stocks_data_collector import list_of_stocks
 from stocks_data_collector import stock_data_functions
+
+
+
+def main():
+    stock_list = list_of_stocks()
+    stock_data_file = f"{stock_list[0]}_{stock_data_functions[1]}.csv"
+    stock_overview_file = f"{stock_list[0]}_{stock_data_functions[0]}.json"
+    overview_data = {}
+
+    with open(stock_overview_file, mode='r') as overview_file:
+        overview_data.update(json.load(overview_file))
+    stock_dataframe = pd.read_csv(stock_data_file)
+
+    #setting timestamp as index of the dataframe
+    stock_dataframe = stock_dataframe.set_index(pd.DatetimeIndex(stock_dataframe['timestamp'].values))
+
+    print('')
+    print("Stock Symbol:",overview_data["Symbol"],"\n")
+    print("Description:",overview_data["Description"],"\n")
+    print("Address:",overview_data["Address"],"\n")
+
+    create_candlestick_chart(stock_dataframe, overview_data["Name"])
 
 def create_candlestick_chart(stock_dataframe, stock_name):
     # Create an interactive candlestick chart
@@ -28,27 +48,6 @@ def create_candlestick_chart(stock_dataframe, stock_name):
         xaxis_title = "Date"
         )
     figure.show()
-
-def main():
-    stock_list = list_of_stocks()
-    stock_data_file = f"{stock_list[0]}_{stock_data_functions[1]}.csv"
-    stock_overview_file = f"{stock_list[0]}_{stock_data_functions[0]}.json"
-    overview_data = {}
-
-    with open(stock_overview_file, mode='r') as overview_file:
-        overview_data.update(json.load(overview_file))
-    stock_dataframe = pd.read_csv(stock_data_file)
-
-    #setting timestamp as index of the dataframe
-    stock_dataframe = stock_dataframe.set_index(pd.DatetimeIndex(stock_dataframe['timestamp'].values))
-
-    print('')
-    print("Stock Symbol:",overview_data["Symbol"],"\n")
-    print("Description:",overview_data["Description"],"\n")
-    print("Address:",overview_data["Address"],"\n")
-
-    create_candlestick_chart(stock_dataframe, overview_data["Name"])
-
 
 if __name__ == "__main__":
     main()
